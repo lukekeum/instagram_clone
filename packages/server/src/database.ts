328fad5a82105a15314660089f762b7cbfebc513
lukekeum <lukekeum11@gmail.com>
2021-04-07 18:39:39 +0900

    feat(server): Add database connection with typeorm

---
+++ packages/server/src/database.ts
@@ -0,0 +1,29 @@
+import { ConnectionOptions, createConnection } from 'typeorm';
+import 'mysql';
+
+class Database {
+  private _connectOptions: ConnectionOptions;
+
+  constructor() {
+    this._setupConnectOptions();
+  }
+
+  private _setupConnectOptions() {
+    this._connectOptions = {
+      type: process.env.TYPEORM_TYPE as any,
+      host: process.env.TYPEORM_HOST,
+      database: process.env.TYPEORM_DATABASE,
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      port: Number(process.env.TYPEORM_PORT || '3306'),
+      synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
+      logging: process.env.TYPEORM_LOGGING === 'true',
+    };
+  }
+
+  public async connect() {
+    return createConnection(this._connectOptions);
+  }
+}
+
+export default Database;
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,7 +2,16 @@ import 'reflect-metadata';
 import './env';
 
 import Server from './server';
+import Database from './database';
 
+const database = new Database();
 const server = new Server();
+const app = server.server;
 
-server.listen();
+database
+  .connect()
+  .then(() => {
+    app.log.info(`Database connected with ${process.env.TYPEORM_TYPE}`);
+    server.listen();
+  })
+  .catch((err) => console.error(err));
