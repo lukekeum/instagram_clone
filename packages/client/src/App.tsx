9eee9e99077faf75729fbc8e6d148417bdc42701
lukekeum <lukekeum11@gmail.com>
2021-04-08 18:57:59 +0900

    feat(route): Add default route for client-side

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+function ProtectedRoute(auth: boolean, Component: React.FC, ...rest: any[]) {
+  return (
+    <Route
+      {...rest}
+      render={() => (auth ? <Component /> : <Redirect to="/login" />)}
+    />
   );
 }
 
