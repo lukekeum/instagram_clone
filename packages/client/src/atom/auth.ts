7783c0f8c263efc6d99d3992c2e62f5d9c2b41d7
lukekeum <lukekeum11@gmail.com>
2021-04-10 01:09:35 +0900

    fix(atom): Change type of auth-atom profile to optional

--- packages/client/src/atom/auth.ts
+++ packages/client/src/atom/auth.ts
@@ -1,13 +1,11 @@
 import { atom, useRecoilState } from 'recoil';
 
-type TStringORNull = string | null;
-
 export interface IAuthAtom {
   authenticated: boolean;
-  profile: {
-    user_id: TStringORNull;
-    user_tag: TStringORNull;
-    display_name: TStringORNull;
+  profile?: {
+    user_id: string;
+    user_tag: string;
+    display_name: string;
   };
 }
 
@@ -15,11 +13,6 @@ export const authAtom = atom<IAuthAtom>({
   key: 'auth',
   default: {
     authenticated: false,
-    profile: {
-      user_id: null,
-      user_tag: null,
-      display_name: null,
-    },
   },
 });
 
