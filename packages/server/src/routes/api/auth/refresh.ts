9dc9986c675a4f4e18e3b7886cd0da77a6552c6f
lukekeum <lukekeum11@gmail.com>
2021-04-17 21:32:16 +0900

    feat(route): Send user data when refresh patch request

--- packages/server/src/routes/api/auth/refresh.ts
+++ packages/server/src/routes/api/auth/refresh.ts
@@ -4,6 +4,7 @@ import { getRepository } from 'typeorm';
 import AuthToken from '@entity/AuthToken.entity';
 import User from '@entity/User.entity';
 import JWT from '@lib/jwt';
+import UserProfile from '@src/entities/UserProfile.entity';
 
 const refreshRoute: FastifyPluginCallback = async (fastify, opts) => {
   /**
@@ -27,6 +28,9 @@ const refreshRoute: FastifyPluginCallback = async (fastify, opts) => {
       tokenEntity.expired = true;
 
       const user = await userRepository.findOne(tokenEntity.fk_user_id);
+      const userProfile = await getRepository(UserProfile).findOne({
+        fk_user_id: user?.id,
+      });
       const tokens = await user?.generateToken();
 
       await tokenRepository.save(tokenEntity);
@@ -40,6 +44,15 @@ const refreshRoute: FastifyPluginCallback = async (fastify, opts) => {
       return res.status(201).send({
         message: 'Successfully regenerated',
         token: tokens?.accessToken!,
+        data: {
+          id: user?.id,
+          user_id: user?.user_id,
+          email: user?.email,
+          profile: {
+            tag: userProfile?.tag,
+            short_bio: userProfile?.short_bio,
+          },
+        },
       });
     } catch (err) {
       fastify.log.error(err);
