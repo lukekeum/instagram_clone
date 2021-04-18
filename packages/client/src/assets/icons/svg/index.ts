6d819524ed6cef3ecd3b2ee038332bbaeb45fbaa
lukekeum <lukekeum11@gmail.com>
2021-04-18 21:48:54 +0900

    feat(icons): Add icons for navbar

---
+++ packages/client/src/assets/icons/svg/index.ts
@@ -0,0 +1,21 @@
+import { ReactComponent as home } from './home.svg';
+import { ReactComponent as homeActive } from './home-active.svg';
+import { ReactComponent as explorer } from './explorer.svg';
+import { ReactComponent as explorerActive } from './explorer-active.svg';
+import { ReactComponent as feed } from './feed.svg';
+import { ReactComponent as feedActive } from './feed-active.svg';
+import { ReactComponent as direct } from './direct.svg';
+import { ReactComponent as directActive } from './direct-active.svg';
+import { ReactComponent as me } from './me.svg';
+
+export {
+  home,
+  homeActive,
+  explorer,
+  explorerActive,
+  feed,
+  feedActive,
+  direct,
+  directActive,
+  me,
+};
