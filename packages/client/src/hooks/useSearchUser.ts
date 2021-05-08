c0acce2cece3ff99ef895ac87337bb5c5790581a
lukekeum <lukekeum11@gmail.com>
2021-05-09 02:16:05 +0900

    feat(hook): Add user search handler

--- packages/client/src/hooks/useSearchUser.ts
+++ packages/client/src/hooks/useSearchUser.ts
@@ -1,53 +1,54 @@
 import { useCallback, useEffect, useState } from 'react';
+import useSearchUserAtom from '../atom/searchUser';
 import client from '../lib/client';
 
 interface IUser {
   id: string;
   user_id: string;
   email: string;
-  profile: {
+  profiles: {
     tag: string;
     short_bio: string;
   };
 }
 
-interface IResult {
-  found: boolean;
+export interface IResult {
   data?: IUser[];
 }
 
 const useSearchUser = () => {
-  const [result, setResult] = useState<IResult>({ found: false });
-  const [loading, setLoading] = useState(false);
+  const [, setResult] = useSearchUserAtom();
   const [error, setError] = useState<any>();
 
-  const searchUser = useCallback(async (input: string) => {
-    if (!input) return;
-    setError(null);
+  const searchUser = useCallback(
+    async (input: string) => {
+      if (!input) return;
+      setError(null);
 
-    try {
-      setLoading(true);
-      const response = await client.get(`/api/user/search?search=${input}`);
-      setLoading(false);
+      try {
+        setResult({ loading: true });
+        const response = await client.get(`/api/user/search?search=${input}`);
+        setResult({ loading: false });
 
-      if (response.data.message) {
-        return setResult({ found: false });
+        if (response.data.message) {
+          return;
+        }
+
+        const userData = response.data.data as IUser[];
+
+        setResult((prev) => ({ ...prev, data: userData }));
+      } catch (err) {
+        setError(err.response.data);
       }
-
-      const userData = response.data.data as IUser[];
-
-      setResult({ found: true, data: userData });
-    } catch (err) {
-      setLoading(false);
-      setError(err.response.data);
-    }
-  }, []);
+    },
+    [setResult],
+  );
 
   useEffect(() => {
-    setResult({ found: false });
-  }, [error]);
+    setResult({ loading: false });
+  }, [setResult]);
 
-  return { searchUser, result, loading, error } as const;
+  return { searchUser, error } as const;
 };
 
 export default useSearchUser;
